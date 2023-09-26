6ae22bde430afd4dec978466a8aed3d23f932a4f
chandra-98a <chandrakalaa2022@gmail.com>
2023-09-26 11:07:21 +0530

    "printing array elements as well as address of the array"

--- src/com/capgemini/arrayList/ArrayExample.java
+++ src/com/capgemini/arrayList/ArrayExample.java
@@ -1,2 +1,14 @@
-package com.capgemini.arrayList;public class ArrayExample {
+package com.capgemini.arrayList;
+
+public class ArrayExample {
+    public static void main(String [] args){
+        int a[]={10,20,30,40,50};
+        //a prints address of the array
+        System.out.println(a);
+
+        //a[i] prints the array elements
+        for (int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+    }
 }
