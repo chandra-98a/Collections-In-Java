dad8ee75f7877d1bbc045ffee659482bf1ec9a21
chandra-98a <chandrakalaa2022@gmail.com>
2023-09-27 12:54:26 +0530

    "Tree set sort the elements in a Sorted Way"

---
+++ src/com/capgemini/set/ProgramTreeOne.java
@@ -0,0 +1,18 @@
+package com.capgemini.set;
+
+import java.util.PriorityQueue;
+import java.util.TreeSet;
+
+public class ProgramTreeOne {
+    public static void main(String [] args) {
+        TreeSet<Object> treeSet = new TreeSet<Object>();
+        treeSet.add(100);
+        treeSet.add(203);
+        treeSet.add(4);
+        treeSet.add(44);
+        treeSet.add(78);
+        treeSet.add(54);
+        treeSet.add(400);
+        System.out.println(treeSet);
+    }
+}
